76e51d70ab87b8bd7c0c0515e1723657ebbe9621
Manh Cuong Pham <94232154+mcpham211@users.noreply.github.com>
2024-10-23 22:42:02 +0700

    Update dequyvaddequyquaylui2.c

--- IT3230/week5/dequyvaddequyquaylui2.c
+++ IT3230/week5/dequyvaddequyquaylui2.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+
+void GenerateCombination(int n, int *arr, int index, int start) {
+    if (index == n){
+        for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+        printf("\n");
+        return;
+    }
+
+    for (int i = start; i <= n; i++) {
+        arr[index] = i;
+        GenerateCombination(n, arr, index + 1, i + 1);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    printf("%d", n);
+    GenerateCombination(n, arr, 0, 1);
+
+    return 0;
+}
 
